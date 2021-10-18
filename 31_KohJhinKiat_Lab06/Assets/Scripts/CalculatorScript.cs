482ad1e970f50501c437db404a68e2a944673a42
JhinKiat <jhinkiat915@gmail.com>
2021-10-18 16:27:01 +0800

    Input added

--- 31_KohJhinKiat_Lab06/Assets/Scripts/CalculatorScript.cs
+++ 31_KohJhinKiat_Lab06/Assets/Scripts/CalculatorScript.cs
@@ -6,6 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class CalculatorScript : MonoBehaviour
 {
+    public InputField InputAmount;
+    public InputField InputValue;
+    public float value;
+    public Toggle KoreanWon;
+    public Toggle TaiwanDollars;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,4 +23,27 @@ public class CalculatorScript : MonoBehaviour
     {
         
     }
+
+    public void calculate()
+    {
+        if(KoreanWon.isOn)
+        {
+            value = float.Parse(InputAmount.text) * 878.29f;
+            InputValue.text = "$" + value;
+        }
+
+        if (TaiwanDollars.isOn)
+        {
+            value = float.Parse(InputAmount.text) * 20.74f;
+            InputValue.text = "$" + value;
+        }
+
+
+    }
+
+    public void clear()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
 }
